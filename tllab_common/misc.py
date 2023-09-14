37bfbc47309c6f7e505427eb08e0b156262711f0
Wim Pomp <w.pomp@nki.nl>
2023-09-14 09:50:55 +0200

    - rename objFromDict to Struct and add make it return None for missing keys. - Add classes to help with cropping and slicing images.

--- tllab_common/misc.py
+++ tllab_common/misc.py
@@ -2,12 +2,76 @@ import os
 import re
 import yaml
 import sys
+import numpy as np
 from traceback import print_exception
 from IPython import embed
 from copy import deepcopy
 from dataclasses import dataclass
 from numbers import Number
 import roifile
+
+
+class Struct(dict):
+    """ dict where the items are accessible as attributes """
+    key_pattern = re.compile(r'(^(?=\d)|\W)')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.update(*args, **kwargs)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __getattr__(self, key):
+        if key not in self:
+            raise AttributeError(key)
+        return self[key]
+
+    def __setitem__(self, key, value):
+        super().__setitem__(self.transform_key(key), value)
+
+    def __getitem__(self, key):
+        return super().__getitem__(self.transform_key(key))
+
+    def __contains__(self, key):
+        return super().__contains__(self.transform_key(key))
+
+    def __deepcopy__(self, memodict=None):
+        cls = self.__class__
+        copy = cls.__new__(cls)
+        copy.update(**deepcopy(super(), memodict or {}))
+        return copy
+
+    def __dir__(self):
+        return self.keys()
+
+    def __missing__(self, key):
+        return None
+
+    @classmethod
+    def transform_key(cls, key):
+        return cls.key_pattern.sub('_', key) if isinstance(key, str) else key
+
+    def copy(self):
+        return self.__deepcopy__()
+
+    def update(self, *args, **kwargs):
+        for arg in args:
+            if hasattr(arg, 'keys'):
+                for key, value in arg.items():
+                    self[key] = value
+            else:
+                for key, value in arg:
+                    self[key] = value
+        for key, value in kwargs.items():
+            self[key] = value
+
+    @staticmethod
+    def construct_yaml_map(y, node):
+        data = Struct()
+        yield data
+        value = y.construct_mapping(node)
+        data.update(value)
 
 
 loader = yaml.SafeLoader
@@ -21,6 +85,10 @@ loader.add_implicit_resolver(
     |[-+]?\\.(?:inf|Inf|INF)
     |\.(?:nan|NaN|NAN))$''', re.X),
     list(r'-+0123456789.'))
+
+loader.add_constructor('tag:yaml.org,2002:python/dict', Struct.construct_yaml_map)
+loader.add_constructor('tag:yaml.org,2002:omap', Struct.construct_yaml_map)
+loader.add_constructor('tag:yaml.org,2002:map', Struct.construct_yaml_map)
 
 
 @dataclass
@@ -63,32 +131,32 @@ class ErrorValue:
 
 
 def save_roi(file, coordinates, shape, columns=None, name=None):
-        if columns is None:
-            columns = 'xyCzT'
-        coordinates = coordinates.copy()
-        if '_' in columns:
-            coordinates['_'] = 0
-        # if we save coordinates too close to the right and bottom of the image (<1 px) the roi won't open on the image
-        if not coordinates.empty:
-            coordinates = coordinates.query(f'-0.5<={columns[0]}<{shape[1]-1.5} & -0.5<={columns[1]}<{shape[0]-1.5} &'
-                                            f' -0.5<={columns[3]}<={shape[3]-0.5}')
-        if not coordinates.empty:
-            roi = roifile.ImagejRoi.frompoints(coordinates[list(columns[:2])].to_numpy().astype(float))
-            roi.roitype = roifile.ROI_TYPE.POINT
-            roi.options = roifile.ROI_OPTIONS.SUB_PIXEL_RESOLUTION
-            roi.counters = len(coordinates) * [0]
-            roi.counter_positions = (1 + coordinates[columns[2]].to_numpy() +
-                                     coordinates[columns[3]].to_numpy().round().astype(int) * shape[2] +
-                                     coordinates[columns[4]].to_numpy() * shape[2] * shape[3]).astype(int)
-            if name is None:
-                roi.name = ''
-            else:
-                roi.name = name
-            roi.version = 228
-            roi.tofile(file)
+    if columns is None:
+        columns = 'xyCzT'
+    coordinates = coordinates.copy()
+    if '_' in columns:
+        coordinates['_'] = 0
+    # if we save coordinates too close to the right and bottom of the image (<1 px) the roi won't open on the image
+    if not coordinates.empty:
+        coordinates = coordinates.query(f'-0.5<={columns[0]}<{shape[1]-1.5} & -0.5<={columns[1]}<{shape[0]-1.5} &'
+                                        f' -0.5<={columns[3]}<={shape[3]-0.5}')
+    if not coordinates.empty:
+        roi = roifile.ImagejRoi.frompoints(coordinates[list(columns[:2])].to_numpy().astype(float))
+        roi.roitype = roifile.ROI_TYPE.POINT
+        roi.options = roifile.ROI_OPTIONS.SUB_PIXEL_RESOLUTION
+        roi.counters = len(coordinates) * [0]
+        roi.counter_positions = (1 + coordinates[columns[2]].to_numpy() +
+                                 coordinates[columns[3]].to_numpy().round().astype(int) * shape[2] +
+                                 coordinates[columns[4]].to_numpy() * shape[2] * shape[3]).astype(int)
+        if name is None:
+            roi.name = ''
+        else:
+            roi.name = name
+        roi.version = 228
+        roi.tofile(file)
 
 
-class color_class(object):
+class Color(object):
     """ print colored text:
             print(color('Hello World!', 'r:b'))
             print(color % 'r:b' + 'Hello World! + color)
@@ -168,31 +236,27 @@ class color_class(object):
     def __repr__(self):
         return self._fmt()
 
-color = color_class()
 
-
-def getConfig(file):
+def get_config(file):
     """ Open a yml parameter file
     """
     with open(file, 'r') as f:
         return yaml.load(f, loader)
 
 
-def getParams(parameterfile, templatefile=None, required=None):
+def get_params(parameterfile, templatefile=None, required=None):
     """ Load parameters from a parameterfile and parameters missing from that from the templatefile. Raise an error when
         parameters in required are missing. Return a dictionary with the parameters.
     """
-    params = getConfig(parameterfile)
-
     # recursively load more parameters from another file
     def more_params(params, file):
-        if not params.get('moreParams') == none():
+        if not params.get('moreParams') is None:
             if os.path.isabs(params['moreParams']):
                 moreParamsFile = params['moreParams']
             else:
                 moreParamsFile = os.path.join(os.path.dirname(os.path.abspath(file)), params['moreParams'])
             print(color(f'Loading more parameters from {moreParamsFile}', 'g'))
-            mparams = getConfig(moreParamsFile)
+            mparams = get_config(moreParamsFile)
             more_params(mparams, file)
             for k, v in mparams.items():
                 if k not in params:
@@ -208,17 +272,6 @@ def getParams(parameterfile, templatefile=None, required=None):
             elif isinstance(value, dict):
                 check_params(params[key], value, f'{path}{key}.')
 
-    # recursively convert string nones to type None
-    def convert_none(params):
-        for key, value in params.items():
-            if value == none():
-                params[key] = None
-            elif isinstance(value, dict):
-                convert_none(value)
-
-    convert_none(params)
-    more_params(params, parameterfile)
-
     def check_required(params, required):
         if required is not None:
             for p in required:
@@ -229,10 +282,12 @@ def getParams(parameterfile, templatefile=None, required=None):
                     if p not in params:
                         raise Exception(f'Parameter {p} not given in parameter file.')
 
+    params = get_config(parameterfile)
+    more_params(params, parameterfile)
     check_required(params, required)
 
     if templatefile is not None:
-        check_params(params, getConfig(templatefile))
+        check_params(params, get_config(templatefile))
     return params
 
 
@@ -249,57 +304,92 @@ def convertParamFile2YML(file):
                 f.write(line+'\n')
 
 
-class objFromDict(dict):
-    """ Usage: objFromDict(**dictionary).
-        Print gives the list of attributes.
-    """
-    def __init__(self, **entries):
-        super(objFromDict, self).__init__()
-        for key, value in entries.items():
-            key = key.replace('-', '_').replace('*', '_').replace('+', '_').replace('/', '_')
-            self[key] = value
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def __getattr__(self, key):
-        if key not in self:
-            raise AttributeError(key)
-        return self[key]
-
-    def __repr__(self):
-        return '** {} attr. --> '.format(self.__class__.__name__)+', '.join(filter((lambda s: (s[:2]+s[-2:]) != '____'),
-                                                                                   self.keys()))
-
-    def copy(self):
-        return self.__deepcopy__()
-
-    def __deepcopy__(self, memodict={}):
-        cls = self.__class__
-        copy = cls.__new__(cls)
-        copy.update(**deepcopy(super(objFromDict, self), memodict))
-        return copy
-
-    def __dir__(self):
-        return self.keys()
-
-
-class none():
-    """ special class to check if an object is some variation of none
-    """
-    def __eq__(self, other):
-        if isinstance(other, none):
-            return True
-        if other is None:
-            return True
-        if hasattr(other, 'lower') and other.lower() == 'none':
-            return True
-        return False
-
-
 def ipy_debug():
     """ Enter ipython after an exception occurs any time after executing this. """
     def excepthook(etype, value, traceback):
         print_exception(etype, value, traceback)
         embed(colors='neutral')
     sys.excepthook = excepthook
+
+
+def get_slice(array, n):
+    if isinstance(n, slice):
+        n = (n,)
+    if isinstance(n, type(Ellipsis)):
+        n = (None,) * array.ndim
+    if isinstance(n, Number):
+        n = (slice(n),)
+    n = list(n)
+    ell = [i for i, e in enumerate(n) if isinstance(e, type(Ellipsis))]
+    if len(ell) > 1:
+        raise IndexError('an index can only have a single ellipsis (...)')
+    if len(ell):
+        if len(n) > array.ndim:
+            n.remove(Ellipsis)
+        else:
+            n[ell[0]] = None
+            while len(n) < array.ndim:
+                n.insert(ell[0], None)
+    while len(n) < array.ndim:
+        n.append(None)
+
+    pad = []
+    for i, (e, s) in enumerate(zip(n, array.shape)):
+        if e is None:
+            e = slice(None)
+        elif isinstance(e, Number):
+            e = slice(e, e)
+        start, stop, step = int(np.floor(e.start or 0)), int(np.ceil(e.stop or s)), round(e.step or 1)
+        if step != 1:
+            raise NotImplementedError('step sizes other than 1 are not implemented!')
+        pad.append((max(0, -start) // step, max(0, stop - s) // step))
+        if start < 0:
+            start = 0
+        elif start >= s:
+            start = s
+        if stop >= s:
+            stop = s
+        elif stop < 0:
+            stop = 0
+        n[i] = slice(start, stop, step)
+    return n, pad
+
+
+@dataclass
+class Crop:
+    """ Special crop object which never takes data from outside the array, and returns the used extent too,
+        together with an image showing how much of each pixel is within the extent,
+        negative indices are taken literally, they do not refer to the end of the dimension!
+    """
+    array: np.ndarray
+
+    def __getitem__(self, n):
+        n = get_slice(self.array, n)[0]
+        return np.vstack([(i.start, i.stop) for i in n]), self.array[tuple(n)]
+
+
+@dataclass
+class SliceKeepSize:
+    """ Guarantees the size of the slice by filling with a default value,
+        negative indices are taken literally, they do not refer to the end of the dimension!
+    """
+    array: np.ndarray
+    default: Number = 0
+
+    def __getitem__(self, n):
+        n, pad = get_slice(self.array, n)
+        return np.pad(self.array[tuple(n)], pad, constant_values=self.default)
+
+    def __setitem__(self, n, value):
+        n = np.vstack(n)
+        idx = np.prod([(0 < i) & (i < s) for i, s in zip(n, self.array.shape)], 0) > 0
+        if not isinstance(value, Number):
+            value = np.asarray(value)[idx]
+        if n.size:
+            self.array[tuple(n[:, idx])] = value
+
+
+color = Color()
+getConfig = get_config
+getParams = get_params
+objFromDict = Struct
